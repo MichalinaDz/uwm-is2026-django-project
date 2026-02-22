e9fb7511d073a39f66a75528b69ce669c7be3f8d
mdziewiatkowska <michalina.dziewiatkowska@edukacja.uwm.edu.pl>
2026-02-22 11:59:34 +0100

    3 commit- po lab 2, połowa lab3

---
+++ blog/posts/models.py
@@ -0,0 +1,12 @@
+from django.db import models
+
+class Category(models.Model):
+    name = models.CharField(max_length=60)
+
+
+class Topic(models.Model):
+    name = models.CharField(max_length=60)
+    category = models.ForeignKey(Category, on_delete=models.CASCADE)
+    created = models.DateTimeField(auto_now_add=True)
+
+# Create your models here.
